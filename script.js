71d652680cda88f3cffd0647298d5a18d57bcfe1
anitha bontha <anitha.a.b@gmail.com>
2022-04-01 12:42:31 +0530

    hackthon-1

--- script.js
+++ script.js
@@ -1,13 +1,73 @@
-async function foo(){
-    let res=document.getElementById("text").value;
-    console.log(res);
-    let result=await fetch(`"https://www.anapioficeandfire.com/api/books"${res}`);
-    let final=await result.json();
-    console.log(final);
-    for(var i=0;i<final.length;i++){
-        // console.log();
-        let ele=document.createElement("div");
-        ele.innerHTML=`Name:${final[i].Name} Author:${final[i].Author} ISBN:${final[i].ISBN} Number of pages:${final[i].Number of pages} ReleaseDate:${final[i].ReleaseDate}`;
-        document.body.append(ele);
+
+
+
+
+
+
+
+
+// async function mul(){
+//     try {
+//     let data=await foo();
+//     let res=await data.json();
+//     console.log(res);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// mul();
+
+// function foo(){
+// return fetch("https://www.anapioficeandfire.com/api/books");
+// }
+
+// api url
+const api_url = 
+      "https://www.anapioficeandfire.com/api/books";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
     }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+  
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = 
+        `<tr>
+          <th>Name</th>
+          <th>No of pages</th>
+          <th>publisher</th>
+          <th>ISBN</th>
+          <th>author</th>
+          
+         </tr>`;
+         
+         for (let r of data) {
+            tab += `<tr> 
+        <td>${r.name} </td>
+        <td>${r.noofpages} </td>
+        <td>${r.publisher}</td> 
+        <td>${r.ISBN}</td>
+        <td>${r.author}</td> 
+                
+    </tr>`;
+}
+document.getElementById("books").innerHTML = tab;
 }
